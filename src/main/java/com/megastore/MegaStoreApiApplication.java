9dd218c3aa7d82c01dc3418c3637e15f8b33aea8
Robson Moraes Cardoso <48536325+RobsonMora@users.noreply.github.com>
2021-04-18 12:43:14 -0300

    First Commit

---
+++ src/main/java/com/megastore/MegaStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.megastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megastore/Controller/ClienteControllerTest.java
@@ -0,0 +1,5 @@
+package com.megastore.Controller;
+
+public class ClienteControllerTest {
+
+}
